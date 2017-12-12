9fde59f7afe346dc70c325f90d448d8b32779824
Danyel Murray and Sam Gespass <danyel@>
2017-12-12 11:40:33 -0800

    add all functionality except poster

---
+++ js/scripts.js
@@ -0,0 +1,94 @@
+/*
+var calculatePrice = function(movie, time, age) {
+var price = 0;
+  if (movie === "new") {
+    price += 36;
+  } else {
+    price += 26;
+  }
+  if (age === "yes") {
+    price *= .85;
+  } else {
+    console.log("age else");
+  }
+  if (student === "yes") {
+    price -= 2;
+  } else {
+    console.log("You are not a student so you are dumb");
+  }
+  if (time === "matinee") {
+    price -= 4;
+  } else {
+    console.log("Wake up earlier!")
+  }
+}*/
+
+var oldArray = ["old room", "ecks", "rocky", "snow", "plan", "reefer", "troll", "gigli", "shark", "xmas", "mac"];
+var newArray = ["emoji", "madea", "thanks", "snowman", "friend", "faith", "flatliners", "wolfcop", "grandmas", "ghost"];
+
+function Tickets(movie, age, student, time, number) {
+  this.movie = movie;
+  this.age = age;
+  this.student = student;
+  this.time = time;
+  this.number = number;
+  this.price = 0;
+  if (this.movie === "new") {
+    this.price += 36;
+  } else {
+    this.price += 26;
+  }
+  if (this.age === "yes") {
+    this.price *= .85;
+  } else {
+    console.log("age else");
+  }
+  if (this.student === "yes") {
+    this.price -= 2;
+  } else {
+    console.log("You are not a student so you are dumb");
+  }
+  if (this.time === "matinee") {
+    this.price -= 4;
+  } else {
+    console.log("Wake up earlier!")
+  }
+  this.price = this.price * this.number;
+  console.log(this.price);
+}
+
+Tickets.prototype.displayTicketPrice = function(){
+  return "Your movie costs $" + Math.round(this.price);
+}
+
+Tickets.prototype.displayPoster = function(){
+  var theMovie = this.movie;
+  console.log(theMovie);
+  oldArray.forEach(function(item){
+    //debugger;
+    if (item === theMovie) {
+      console.log(theMovie);
+      debugger;
+      return "<br><img src='img/" + this.movie + ".jpg' />";
+    }
+  }) /*
+  if (this.movie === "old room") {
+    return "<br><img src='img/old room.jpg' />"
+  } */
+}
+
+$(document).ready(function(){
+  $("form#theForm").submit(function(event){
+    event.preventDefault();
+    $("#result").empty();
+    var movie = $("#movie").val();
+//    var name = $("#movie[name]").val();
+    var age = $("input:radio[name=age]:checked").val();
+    var student = $("input:radio[name=student]:checked").val();
+    var time = $("input:radio[name=time]:checked").val();
+    var number = $("input.number").val();
+    var newTickets = new Tickets(movie, age, student, time, number);
+    $("#result").append(newTickets.displayTicketPrice());
+    $("#result").append(newTickets.displayPoster());
+  });
+});
---
+++ readme.md
@@ -0,0 +1,54 @@
+# _Movie Tickets_
+
+#### This is a website designed to return user's numerical input with robotic responses, Version 1: 08/12/17
+
+#### By: _**Danyel Murray**_ & _**Sam Gespass**_
+
+
+## Description
+
+
+
+
+## Specifications
+
+Program can return movies price
+
+Program can return price for non-first release movies
+
+Program can return price for matinee showings
+
+Program can return price for age of customer
+
+Program can add multiple discounts to regular priced tickets
+
+Program can return final cost to user
+
+
+
+
+
+## Technologies used:
+
+-html5
+
+-Css
+
+-JavaScript
+
+-Jquery
+
+
+#### Known bugs:
+
+Please alert to any bugs in my program
+
+##### **Contact Details:**
+
+**Danyel: danyelmurray1987@gmail.com**
+
+#### License
+
+_**Copyright (C) 2017 Danyel Murray & Sam Gespass**_
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
